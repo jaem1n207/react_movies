9dd2721667ce57f4c6c02ceeff4524119c20b6b7
jaem1n207 <jaem1n207@naver.com>
2020-05-01 01:06:53 +0900

    [ADD] boxOffice actions

--- ts-react-movies/src/modules/boxOffice/actions.ts
+++ ts-react-movies/src/modules/boxOffice/actions.ts
@@ -1,1 +1,15 @@
-import { createAction } from "typesafe-actions";
+import { createAsyncAction } from "typesafe-actions";
+import { MovietodayInfo } from "../../api/movies";
+import { AxiosError } from "axios";
+
+export const GET_MOVIE_TODAY_INFO = "boxOffice/GET_MOVIE_INFO";
+export const GET_MOVIE_TODAY_INFO_SUCCESS =
+  "github/GETMOVIE_TODAY_INFO_SUCCESS";
+export const GET_MOVIE_TODAY_INFO_ERROR =
+  "boxOffice/GET_MOVIE_TODAY_INFO_ERROR";
+
+export const getMovieTodayInfoAsync = createAsyncAction(
+  GET_MOVIE_TODAY_INFO,
+  GET_MOVIE_TODAY_INFO_SUCCESS,
+  GET_MOVIE_TODAY_INFO_ERROR
+)<string, MovietodayInfo, AxiosError>();
